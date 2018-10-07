5ad20b68e167112befa755ef975a538854ab42c3
Sebastian Montoya <kikonmontoya97@gmail.com>
2018-10-07 18:42:05 -0500

    Update _DELAYS_H.h

---
+++ _DELAYS_H.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   _DELAYS_H.h
+ * Author: Ddrag
+ *
+ * Created on 11 de septiembre de 2018, 21:23
+ */
+
+#ifndef _DELAYS_H_H
+#define	_DELAYS_H_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _DELAYS_H_H */
+
